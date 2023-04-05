843e0d7de2e80d36c8e9a4f91b21690afc427201
inga-divra <ingady@gmail.com>
2023-04-05 18:23:51 +0300

    added product section

--- app/js/main.js
+++ app/js/main.js
@@ -1,19 +1,25 @@
 $(function () {
+  $('.slider').slick({
+    dots: true,
+    arrows: false,
+    autoplay: true,
+    fade: true,
+    cssEase: 'linear',
+    autoplaySpeed: 0,
+    speed: 4000,
+    infinite: true,
+  });
 
-    $('.slider').slick({
-        dots: true,
-        arrows: false,
-        autoplay: true,
-        fade: true,
-        "cssEase": "linear",
-        "autoplaySpeed": 0,
-        "speed": 4000,
-        infinite: true
-    })
+  $('.stars').rateYo({
+    normalFill: '#ccccce',
+    ratedFill: '#ffc35b',
+    starWidth: '20px',
+    halfStar: true,
+    readOnly: true,
+  });
 });
 
-
 // set year
-const date = document.querySelector("#date");
+const date = document.querySelector('#date');
 
 date.innerHTML = new Date().getFullYear();
