ab634782ad3d51dd03ebda659be42d42eb24f1f4
inga-divra <ingady@gmail.com>
2023-04-22 12:10:47 +0300

    added filter js function to shop.html

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,14 @@
 $('.filter-price__input').ionRangeSlider({
   type: 'double',
   prefix: '€ ',
+  onStart: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
+  onChange: function (data) {
+    $('.filter-price__from').text(data.from);
+    $('.filter-price__to').text(data.to);
+  },
 });
 
 $(function () {
